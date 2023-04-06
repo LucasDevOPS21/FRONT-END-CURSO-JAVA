ae9099b4e440b95ba6cdded3941bd573f797c0a3
Jhonathan Pinheiro <128189860+LucasDevOPS21@users.noreply.github.com>
2023-04-05 21:33:43 -0300

    jssite

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ $(document).ready(myApp)
 
 function myApp() {
 
-    console.log('Funciona')
+    // carrega a página inival.
+    loadpage('homb')
+}
+
+// Carrega uma página
+function loadpage(page) {
+
+// Monta os caminhos para os componenetes da página solicitada.//
+const path = {
+    html: `/pages/${page}/index.html`,
+    css: `/pages/${page}/index.css`,
+    js: `/pages/${page}/index.js`
+
+}
+
+$.get(path)
+        .done((data) => {
+            $('main').html(data)
+        })
+        .fail((error) => {
+            loadpage('e404')
+        })
 
 
 }
